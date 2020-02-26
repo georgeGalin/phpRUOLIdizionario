c9a8824c6c569339acee61fe287d4a05bc38aecd
georgeGalin <papezoor.gamer@gmail.com>
2020-02-26 16:55:46 +0100

    ultima

--- index.php
+++ index.php
@@ -6,13 +6,20 @@
     <title>login con ruoli GG </title>
   </head>
   <body>
+
+  <div style="width:100%;max-width:300px;border-radius:10px;border:1px red solid;overflow:hidden">
+<div style="background-color:#eeeeee;padding:5px">
+
+
+    <h2>Registrazione</h2>
       <form action="signup.php" method="POST">          
         Username : <input type="text" name="Nome">
-        Password : <input type="password" name="Password">
-        Ruolo : <select name="role">
+        Password : <input type="password" name="Password"><br><br>
+        Ruolo : <select name="role"><br>
           <option value=0>Admin</option>
           <option value=1>Utente</option>
-          </select>
+          </select><br><br>
+          <h2>Login</h2>
         <button type="submit"> Registrati </button>
       </form>
       <form action="login.php" method="POST">       
@@ -20,5 +27,7 @@
         Password : <input type="password" name="Password">
         <button type="submit">Accedi</button> 
       </form>
+      </div>
+</div>
   </body>
 </html>
--- main.php
+++ main.php
@@ -1,35 +1,43 @@
 <html>         
   <title>Dizionario</title>
-  <h1>Cosa vuoi fare?</h1>
+  <h1>Dizionario</h1>
   <body>
+  <div style="width:100%;max-width:300px;border-radius:10px;border:1px red solid;overflow:hidden">
+<div style="background-color:#eeeeee;padding:5px">
+
+  
       <form action="aggiungi.php" method="POST">          
-        Parola : <input type="text" name="parola">   
+        Parola : <input type="text" name="parola"><br>   
         Significato : <input type="text" name="significato">
-        <button type="submit"> Aggiungi </button>
+        <button type="submit"> Aggiungi </button><br><br>
       </form>
 
 
   
   
       <form action="modifica.php" method="POST">
-        Parola : <input type="text" name="parola">
+        Parola : <input type="text" name="parola"><br>
         Significato : <input type="text" name="significato">  
         <button type = "submit"> Modifica </button>
-      </form>
+      </form><br><br>
 
       <form action="elimina.php" method="POST">
-        Parola : <input type="text" name="parola">
+        Parola : <input type="text" name="parola"><br>
         <button type = "submit"> Elimina </button>
-      </form>
+      </form><br>
  
 
       <form action="visualizza.php" method="POST">    
         Parola : <input type="text" name="parola">   
         <button type = "submit"> Visualizza </button>
-      </form>
+      </form><br><br>
 
       <form action="logout.php" method="POST">        
         <button type = "submit"> Logout </button>
       </form>
+
+      
+</div>
+</div>
   </body>
 </html>   

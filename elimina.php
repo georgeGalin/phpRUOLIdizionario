eb2e04409a92a81590dd051c47d5c74e74caa761
georgeGalin <papezoor.gamer@gmail.com>
2020-02-26 16:28:41 +0100

    finale

--- elimina.php
+++ elimina.php
@@ -1,17 +1,17 @@
-<?php
+    <?php
     session_start();
-    $parola = $_POST["parola"];   
+    $parola = $_POST["parola"];  
     $significato =$_POST["significato"];
     $nome = $_SESSION["Nome"];
     $ruolo = $_SESSION["Role"];
-    $connessione = new mysqli("remotemysql.com:3306", "eoC2i6ecss", "e82eOPGCxU", "eoC2i6ecss"); 
-    if($ruolo == 0) {   
-        $connessione->query("UPDATE `dbparola` SET `Parola`='$parola',`Significato`='$significato' WHERE Parola='$parola'");  
-        echo "la parola inserita è stata eliminata";
-    }elseif($ruolo > 0){
-            echo "Non hai i poteri per eliminare o modificare";
-        }
-        
+    $connessione = new mysqli("remotemysql.com:3306", "eoC2i6ecss", "e82eOPGCxU", "eoC2i6ecss");
+    if($ruolo == 0){
+        $connessione->query("DELETE FROM `dbparola` WHERE Parola = '$parola'");   
+    }
+    else{
+        $connessione->query("DELETE FROM `dbparola` WHERE Parola = '$parola' AND Username = $nome");
+    }
+        echo "parola eliminata";
 ?>
     <form action="main.php" method="POST">       
         <button type="submit">OK!</button>
--- visualizza.php
+++ visualizza.php
@@ -1,18 +1,17 @@
 <?php
-$parola = $_POST["parola"];  
+$parola = $_POST["parola"];
 $connessione = new mysqli("remotemysql.com:3306", "eoC2i6ecss", "e82eOPGCxU", "eoC2i6ecss");
-$sql = "SELECT * FROM `dbparola` WHERE parola = '$parola'";    
+$sql = "SELECT * FROM `dbparola` WHERE parola = '$parola'";  
 $result = mysqli_query($connessione, $sql);
 if (mysqli_num_rows($result) == 0) {
-    echo "Parola mancante nel database";
+    echo "La parola non trovata";
 } else {
     while ($row = mysqli_fetch_assoc($result)) {
-        echo $row["Significato"];      
+        echo $row["significato"];  //risultato    
     }
 }
-mysqli_close($connessione);             
+mysqli_close($connessione);          
 ?>
 <form action="main.php" method="POST">
     <button type="submit">OK!</button>
 </form>
-
